ea88afc4cdd79672510f4e2f5e2250415647c353
Alexander <zleepy2002@gmail.com>
2024-03-27 18:46:04 -0600

    Terminado de 3 secciones con info preliminar

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const mobileNav = document.querySelector('.mobile-navigation ul');
+
+document.querySelector(".mobile-navigation i").addEventListener("click", ()=> {
+    console.log("click")
+    mobileNav.classList.contains("show") ? mobileNav.classList.remove("show") : mobileNav.classList.add("show")
+});
+
+
+
 ScrollReveal().reveal(".sofa-row");
 ScrollReveal().reveal(".our-mission");
-
